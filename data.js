653578a1c5c2cb8ed292b39aa398c600597eeb5d
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-17 16:23:47 +0100

    Feat: General settings

---
+++ data.js
@@ -0,0 +1,40 @@
+const cities = [
+    {
+        id: 1,
+        title: "New York",
+        content: "lorem ipsum",
+        image: "imgs/cities/NewYork.png",
+        tags: ["newYork"],
+    },
+    {
+        
+        id: 2,
+        title: "Paris",
+        content: "lorem ipsum",
+        image: "imgs/cities/Paris.png",
+        tags: ["paris"],
+    },
+    {
+        id: 3,
+        title: "Madrid",
+        content: "lorem ipsum",
+        image: "imgs/cities/Madrid.png",
+        tags: ["madrid"],
+    },
+    {
+        id: 4,
+        title: "London",
+        content: "lorem ipsum",
+        image: "imgs/cities/London.png",
+        tags: ["london"],
+    },
+    {
+        id: 5,
+        title: "Rome",
+        content: "lorem ipsum",
+        image: "imgs/cities/Rome.png",
+        tags: ["rome"],
+    }
+
+]
+export default cities;
