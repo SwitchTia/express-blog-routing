8f2c97c4ed3a29053d262a5e0bf4e1f890a1ae3d
SwitchTia <oblasser.tatiana@gmail.com>
2025-12-17 22:51:57 +0100

    Fix: debug

--- routers/citiesRouter.js
+++ routers/citiesRouter.js
@@ -1,68 +1,115 @@
 import express from "express";
-import { cityBlogArray} from "../data.js";
+import { cityBlogArray } from "../data.js";
 
 
 
 const router = express.Router();
 
+
+
 //INDEX
-router.get("/", (req, res)=>{
-    const response = {
-        totalCitiesToVisit: cityBlogArray.length,
-        listOfCities: cityBlogArray,
-    }
-    res.json (response)
+router.get("/", (req, res) => {
+  const response = {
+
+    totalCitiesToVisit: cityBlogArray.length,
+
+    listOfCities: cityBlogArray,
+  }
+  res.json(response)
 })
+
 
 
 //SHOW
-router.get("/:id",(req,res)=>{
-    const id = parseInt(req.params.id);
-    const resp = cityBlogArray.find(city => city.id===id);
-    res.json (resp)
+router.get("/:id", (req, res) => {
+
+  const id = parseInt(req.params.id);
+
+
+  const resp = cityBlogArray.find(city => city.id === id);
+
+  res.json(resp)
 })
+
+
 
 // STORE
 router.post("/", (req, res) => {
 
   const newCity = req.body;
 
-  //generating a new id:
-  const newId = cityBlogArray.length > 0 
-    ? Math.max(...cityBlogArray.map(city => city.id)) + 1 
-    : 1;
 
   //creating a new blog object:
-  const cityBlogToAdd  = {
+  const newCityBlogToAdd = {
     id: newId,
     title: newCity.title,
     content: newCity.content,
-    // image: newCity.image,
-    // tags: newCity.tags
+    // image: newCity.image,?
+    tags: newCity.tags
   }
-  
+
   cityBlogArray.push(cityToAdd);
 
-  res.json(cityBlogToAdd);
+  res.json(newCityBlogToAdd);
 
 });
 
+
+
 // UPDATE
 router.put("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+
+  //updated fields
 
   res.json("Modify you blog");
 });
 
 
+
+
+//MODIFY
+router.patch("/:id", (req, res) => {
+
+  const id = parseInt(req.params.id);
+
+
+  const city = cityBlogArray.find(city => city.id === id);
+
+
+  const updates = req.body;
+
+  if (updates.title) {
+    city.title = updates.title;
+  }
+  if (updates.content) {
+    city.content = updates.content;
+  }
+  if (updates.image) {
+    city.image = updates.image;
+  }
+  if (updates.tags) {
+    city.tags = updates.tags;
+  }
+
+
+  res.json(city);
+
+});
+
+
+
 //DESTROY
 router.delete("/:id", (req, res) => {
+
   const id = parseInt(req.params.id);
+
 
   const cityIndex = cityBlogArray.findIndex(city => city.id === id);
 
-  const deletedCity = cityBlogArray.splice(cityId, 1);
+  const deletedCity = cityBlogArray.splice(cityIndex, 1);
 
-  res.json("CityBlog " + deletedCity[0]);
+  res.json("CityBlog " + deletedCity + "has been removed permanently");
 });
 
 export default router;
